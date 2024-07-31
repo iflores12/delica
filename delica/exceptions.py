265f190990f2f0f9abde9d2b9354e1eb2fc7aaba
Isaac Flores-Huerta <isaacflores@outlook.com>
2024-07-31 00:17:32 -0400

    linting

--- delica/exceptions.py
+++ delica/exceptions.py
@@ -1,6 +1,22 @@
+from typing import Any
+
+
 class DelicaException(Exception):
     """Base Exception For API"""
 
 
 class MissingRequiredException(DelicaException):
     """Exception Raised When You're Missing Something That Is Required"""
+
+
+class RequestException(DelicaException):
+    def __init__(self, og_exception: Exception, request_args: tuple[Any, ...], request_kwargs: Any):
+        self.og_exception = og_exception
+        self.request_args = request_args
+        self.request_kwargs = request_kwargs
+
+        super().__init__(f"Error Making Request: {og_exception}")
+
+
+class VanApiException(DelicaException):
+    """Error Messages from VAN API"""
