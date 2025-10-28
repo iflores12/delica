0d8350b72b634a0aab5a63aeb0023c1d365b7207
Isaac Flores-Huerta <isaac@flores-huerta.com>
2025-10-27 21:27:49 -0400

    begin work on people routes

--- delica/models/people.py
+++ delica/models/people.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
-from delica.models.baseModel import DelicaBaseModel
+
+from datetime import date
+
 from delica.endpoints import API_URL
 from delica.exceptions import DelicaException
+from delica.models.baseModel import DelicaBaseModel
 
 
 class People(DelicaBaseModel):
@@ -12,10 +15,18 @@ class People(DelicaBaseModel):
         """
         super().__init__(delica, _data=None)
 
-    def find(self, person_id: int):
+    def find(self, *, person_query: dict | None = None):
+        if person_query is None:
+            raise DelicaException("Must provide a person query object to search")
+
+        return self._delica.get(API_URL["find"], data=person_query)
+
+    def find_person(self, person_id: int):
         if person_id:
             route = API_URL["find_people"]
         else:
-            raise DelicaException
+            raise DelicaException("Person ID required")
 
-        return self._delica.get(route)
+        data = {"vanId": person_id}
+
+        return self._delica.post(route, data=data)
